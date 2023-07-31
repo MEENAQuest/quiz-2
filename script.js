3824e308ebd289758391f498f2240a3a53a5d9ec
MEENAQuest <135314500+MEENAQuest@users.noreply.github.com>
2023-07-31 07:46:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var ques_number=0;
+var total_score=0;
+var current_score=0;
+var description;
+var spirit_animal;
+
+var myQuestions=['JavaScript','HTML','CSS','Bootstrap'];
+var number_questions=myQuestions.length;
+
+alert(number_questions)
